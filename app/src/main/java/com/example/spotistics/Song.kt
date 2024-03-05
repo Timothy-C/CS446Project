0d2bcc089b7bfa8112871275e5022ac840dd1b67
hyeminrho127 <hyeminrho127@gmail.com>
2024-03-05 04:13:05 -0500

    Throwbacks Screen created

--- app/src/main/java/com/example/spotistics/Song.kt
+++ app/src/main/java/com/example/spotistics/Song.kt
@@ -13,6 +13,19 @@ val dummySongs = listOf(
     Song(3, R.drawable.bohemian_rhapsody, "Bohemian Rhapsody", "Queen"),
     Song(4, R.drawable.bohemian_rhapsody, "Bohemian Rhapsody", "Queen"),
     Song(5, R.drawable.bohemian_rhapsody, "Bohemian Rhapsody", "Queen"),
+    Song(6, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+    Song(7, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+    Song(8, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+    Song(9, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+    Song(10, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+)
+
+val dummySongs2 = listOf(
+    Song(1, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+    Song(2, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+    Song(3, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+    Song(4, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
+    Song(5, R.drawable.taylor_swift, "Lover", "Taylor Swift"),
     Song(6, R.drawable.bohemian_rhapsody, "Bohemian Rhapsody", "Queen"),
     Song(7, R.drawable.bohemian_rhapsody, "Bohemian Rhapsody", "Queen"),
     Song(8, R.drawable.bohemian_rhapsody, "Bohemian Rhapsody", "Queen"),
