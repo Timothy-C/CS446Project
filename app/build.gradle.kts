20044658fdb1a62c342d639f796af3d6ef0aac9f
amywang480 <amywang480@gmail.com>
2024-03-05 19:35:44 -0500

    Added home screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui-graphics")
+    implementation("com.google.android.gms:play-services-cronet:18.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- timelog.md
+++ timelog.md
@@ -5,3 +5,8 @@
 |2024/01/30|0.25−|−−−−−−−|−−−−−−−−|−−−−−|-------|--------| Setup readme and timelog
 
 |2024/02/06|-----|−−−−−−−|−−−−−−−−|0.25-|-------|--------| Setup Android Jetpack Compose project
+
+|2024/02/21|-----|−−−−−−−|−−−−−−−−|1----|-------|--------| Setup top app bar, navigation, and home screen structure
+
+|2024/03/01|-----|−−−−−−−|−−−−−−−−|3----|-------|--------| Develop and design home screen with side navigation panel
+
