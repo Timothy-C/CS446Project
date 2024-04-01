0ed4b334b57ef3d110cde3c94bc9e8c680fc665b
tracy! <55264469+1tracy@users.noreply.github.com>
2024-04-01 17:20:56 -0400

    add spotify auth functions

--- app/src/main/java/com/example/spotistics/MainActivity.kt
+++ app/src/main/java/com/example/spotistics/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.spotistics
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
+import android.view.View
+import android.widget.TextView
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -44,18 +47,30 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.spotistics.ui.theme.Navy
 import com.example.spotistics.ui.theme.quicksandFamily
+import com.google.android.material.snackbar.Snackbar
 import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
 import kotlinx.coroutines.launch
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
 
 object Screens {
     const val Home = "home"
@@ -66,19 +81,132 @@ object Screens {
     const val Settings = "settings"
 }
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private val clientId = "dcb7c8ef25dd48c2b832fd73164d9f4c"
-    private val redirectUri = "http://localhost:3000/auth/callback"
+//    private val redirectUri = "http://localhost:3000/auth/callback"
+    private val mOkHttpClient = OkHttpClient()
+    private var mAccessToken: String? = null
+    private var mAccessCode: String? = null
+    private var mCall: Call? = null
     private var spotifyAppRemote: SpotifyAppRemote? = null
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("mainactivity", "mainactivity")
         setContent {
             actionBar?.hide()
             Navigation()
         }
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        cancelCall()
+        super.onDestroy()
+    }
+
+    fun onGetUserProfileClicked(view: View?) {
+        if (mAccessToken == null) {
+            val snackbar = Snackbar.make(
+                findViewById(R.id.activity_main),
+                R.string.warning_need_token,
+                Snackbar.LENGTH_SHORT
+            )
+            snackbar.view.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
+            snackbar.show()
+            return
+        }
+        val request: Request = Request.Builder()
+            .url("https://api.spotify.com/v1/me")
+            .addHeader("Authorization", "Bearer $mAccessToken")
+            .build()
+        cancelCall()
+        mCall = mOkHttpClient.newCall(request)
+        mCall!!.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                setResponse("Failed to fetch data: $e")
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                try {
+                    val jsonObject = JSONObject(response.body!!.string())
+                    setResponse(jsonObject.toString(3))
+                } catch (e: JSONException) {
+                    setResponse("Failed to parse data: $e")
+                }
+            }
+        })
+    }
+
+    fun onRequestCodeClicked(view: View?) {
+        val request = getAuthenticationRequest(AuthorizationResponse.Type.CODE)
+        AuthorizationClient.openLoginActivity(this, AUTH_CODE_REQUEST_CODE, request)
+    }
+
+    fun onRequestTokenClicked(view: View?) {
+        val request = getAuthenticationRequest(AuthorizationResponse.Type.TOKEN)
+        AuthorizationClient.openLoginActivity(this, AUTH_TOKEN_REQUEST_CODE, request)
+    }
+
+    private fun getAuthenticationRequest(type: AuthorizationResponse.Type): AuthorizationRequest {
+        return AuthorizationRequest.Builder(CLIENT_ID, type, redirectUri.toString())
+            .setShowDialog(false)
+            .setScopes(arrayOf("user-read-email"))
+            .setCampaign("your-campaign-token")
+            .build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val response = AuthorizationClient.getResponse(resultCode, data)
+        if (response.error != null && !response.error.isEmpty()) {
+            setResponse(response.error)
+        }
+        if (requestCode == AUTH_TOKEN_REQUEST_CODE) {
+            mAccessToken = response.accessToken
+            updateTokenView()
+        } else if (requestCode == AUTH_CODE_REQUEST_CODE) {
+            mAccessCode = response.code
+            updateCodeView()
+        }
+    }
+
+    private fun setResponse(text: String) {
+        runOnUiThread {
+            val responseView = findViewById<TextView>(R.id.response_text_view)
+            responseView.text = text
+        }
+    }
+
+    private fun updateTokenView() {
+        val tokenView = findViewById<TextView>(R.id.token_text_view)
+        tokenView.text =
+            getString(R.string.token, mAccessToken)
+    }
+
+    private fun updateCodeView() {
+        val codeView = findViewById<TextView>(R.id.code_text_view)
+        codeView.text =
+            getString(R.string.code, mAccessCode)
+    }
+
+    private fun cancelCall() {
+        if (mCall != null) {
+            mCall!!.cancel()
+        }
+    }
+
+    private val redirectUri: Uri
+        private get() = Uri.Builder()
+            .scheme(getString(R.string.com_spotify_sdk_redirect_scheme))
+            .authority(getString(R.string.com_spotify_sdk_redirect_host))
+            .build()
+
+    companion object {
+        const val CLIENT_ID = "dcb7c8ef25dd48c2b832fd73164d9f4c"
+        const val AUTH_TOKEN_REQUEST_CODE = 0x10
+        const val AUTH_CODE_REQUEST_CODE = 0x11
     }
 }
 
